da52d65494e5ff262e65ece85c41b8193986e505
Konstantin <99410214+kosttiq@users.noreply.github.com>
2023-02-18 16:04:39 +0300

    PythonTutorLesson3

---
+++ pythontutorLesson3.py
@@ -0,0 +1,21 @@
+# LESSON 3 VICHISLENIYA
+
+print(17/3)
+Result = 5.6666666666667
+print(17//3)
+Result = 5
+print(17%3)
+Result = 2
+
+#SUM TSIFR
+a = int(input())
+b = a//100
+c = a//10%10
+d = a%10
+print(int(b+c+d))
+
+#GPOTENUZA
+import math
+a = int(input())
+b = int(input())
+print(math.sqrt(a**2+b**2))
